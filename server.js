a219d153a55e9a2fe2f4354008db79e74191836a
Jas Mann <jasforshort@hotmail.ca>
2022-11-24 01:19:25 +0000

    Added working routes for register and login

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5000;
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 // DB Setup
 const { Pool } = require("pg");
@@ -16,20 +17,23 @@ db.connect();
 app.use(express.json()); //req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(cors());
 
 // Routing
 const landing_page = require("./routes/landing_page")(db);
 const login = require("./routes/login")
 const logout = require("./routes/logout");
-const registration = require("./routes/registration");
+const register = require("./routes/register");
 const filter = require("./routes/filter")(db);
+const auth = require("./routes/jwtAuth");
 
 // //Mount Resourceß
 app.use("/", landing_page);
 app.use("/login", login);
 app.use("/logout", logout);
-app.use("/registration", registration);
+app.use("/register", register);
 app.use("/filter", filter);
+app.use("/auth", auth(db));
 
 app.listen(PORT, () => {
   console.log("server has started listening on port ", PORT)
