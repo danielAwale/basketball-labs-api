77a8490ea37ace2483902428a5e1c1f41c3c94b1
mjuned <mmj.nowaz@gmail.com>
2022-12-01 19:25:06 +0000

    ignore

--- routes/filter.js
+++ routes/filter.js
@@ -7,7 +7,7 @@ module.exports = function(db) {
   router.get("/", async (req, res) => {
     try {
       const getAllStats = await db.query('SELECT * FROM players')
-      console.log(res.json(getAllStats.rows))
+      res.json(getAllStats.rows)
     } catch (error) {
       console.log(error.message)
     }
@@ -15,8 +15,8 @@ module.exports = function(db) {
 
   router.get("/points", async (req, res) => {
     try {
-      const filterByPoints = await db.query('SELECT * FROM players ORDER BY points DESC')
-      console.log(res.json(filterByPoints.rows))
+      const filterByPoints = await db.query('SELECT points FROM players ORDER BY points DESC')
+      res.json(filterByPoints.rows)
     } catch (error) {
       console.log(error.message)
     }
@@ -24,8 +24,8 @@ module.exports = function(db) {
 
   router.get("/assists", async (req, res) => {
     try {
-      const filterByAssists = await db.query('SELECT * FROM players ORDER BY assists DESC')
-      console.log(res.json(filterByAssists.rows))
+      const filterByAssists = await db.query('SELECT assists FROM players ORDER BY assists DESC')
+      res.json(filterByAssists.rows)
     } catch (error) {
       console.log(error.message)
     }
@@ -33,8 +33,8 @@ module.exports = function(db) {
 
   router.get("/rebounds", async (req, res) => {
     try {
-      const filterByRebounds = await db.query('SELECT * FROM players ORDER BY rebounds DESC')
-      console.log(res.json(filterByRebounds.rows))
+      const filterByRebounds = await db.query('SELECT rebounds FROM players ORDER BY rebounds DESC')
+      res.json(filterByRebounds.rows)
     } catch (error) {
       console.log(error.message)
     }
@@ -42,8 +42,8 @@ module.exports = function(db) {
 
   router.get("/steals", async (req, res) => {
     try {
-      const filterBySteals = await db.query('SELECT * FROM players ORDER BY steals DESC')
-      console.log(res.json(filterBySteals.rows))
+      const filterBySteals = await db.query('SELECT steals FROM players ORDER BY steals DESC')
+      res.json(filterBySteals.rows)
     } catch (error) {
       console.log(error.message)
     }
@@ -52,8 +52,8 @@ module.exports = function(db) {
 
   router.get("/blocks", async (req, res) => {
     try {
-      const filterByBlocks = await db.query('SELECT * FROM players ORDER BY blocks DESC')
-      console.log(res.json(filterByBlocks.rows))
+      const filterByBlocks = await db.query('SELECT blocks FROM players ORDER BY blocks DESC')
+      res.json(filterByBlocks.rows)
     } catch (error) {
       console.log(error.message)
     }
@@ -61,8 +61,8 @@ module.exports = function(db) {
 
   router.get("/turnovers", async (req, res) => {
     try {
-      const filterByTurnovers = await db.query('SELECT * FROM players ORDER BY turnovers ASC')
-      console.log(res.json(filterByTurnovers.rows))
+      const filterByTurnovers = await db.query('SELECT turnovers FROM players ORDER BY turnovers ASC')
+      res.json(filterByTurnovers.rows)
     } catch (error) {
       console.log(error.message)
     }
@@ -70,8 +70,8 @@ module.exports = function(db) {
 
   router.get("/fgp", async (req, res) => {
     try {
-      const filterByFGP = await db.query('SELECT * FROM players ORDER BY field_goal_percentage DESC')
-      console.log(res.json(filterByFGP.rows))
+      const filterByFGP = await db.query('SELECT field_goal_percentage FROM players ORDER BY field_goal_percentage DESC')
+      res.json(filterByFGP.rows)
     } catch (error) {
       console.log(error.message)
     }
@@ -79,8 +79,8 @@ module.exports = function(db) {
 
   router.get("/tpm", async (req, res) => {
     try {
-      const filterByTPM = await db.query('SELECT * FROM players ORDER BY three_points_made DESC')
-      console.log(res.json(filterByTPM.rows))
+      const filterByTPM = await db.query('SELECT three_points_made FROM players ORDER BY three_points_made DESC')
+      res.json(filterByTPM.rows)
     } catch (error) {
       console.log(error.message)
     }
@@ -88,8 +88,8 @@ module.exports = function(db) {
 
   router.get("/ftp", async (req, res) => {
     try {
-      const filterByFTP = await db.query('SELECT * FROM players ORDER BY free_throw_percentage DESC')
-      console.log(res.json(filterByFTP.rows))
+      const filterByFTP = await db.query('SELECT free_throw_percentage FROM players ORDER BY free_throw_percentage DESC')
+      res.json(filterByFTP.rows)
     } catch (error) {
       console.log(error.message)
     }
